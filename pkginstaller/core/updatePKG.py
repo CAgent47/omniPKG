1cddb3038c474e56d040c0aca37267f347bde6fd
C.Agent_47 <linux@localhost.localdomain>
2026-07-12 18:46:04 +0330

    created new python file in core folder for update & Upgrade System

---
+++ pkginstaller/core/updatePKG.py
@@ -0,0 +1,23 @@
+import shutil
+import json
+
+def setPackageManager(manager):
+    if shutil.which(manager):
+        return manager
+
+user_package_Manager = None
+
+def usePkgManager(managers):
+    global user_package_Manager
+    for key, Value in managers.items():
+        if setPackageManager(key):
+            user_package_Manager = key
+            break
+
+
+with open('distroPKG.json', 'r') as PackageManager:
+    pkg_Managers = json.load(PackageManager)
+
+
+usePkgManager(pkg_Managers)
+print(pkg_Managers[user_package_Manager]["update"])
