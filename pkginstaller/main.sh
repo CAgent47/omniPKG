cfa9ee50883a63901df10f132bb1b88037eab60f
C.Agent_47 <linux@localhost.localdomain>
2026-07-13 18:00:25 +0330

    Fixing main script

--- pkginstaller/main.sh
+++ pkginstaller/main.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 python3 core/createJson.py
-# eval $(python3 core/updatePKG.py)
-eval $(python3 core/installPKG.py) cowsay
+
+eval $(python3 )
+
+if ! command -v jq &> /dev/null; then 
+    eval $(python3 core/installPKG.py) jq
+fi
+
+
+
 # Created By CAgent_47
