26ffd80d0ae04fdef0adecb334db29ecb0562602
C.Agent_47 <linux@localhost.localdomain>
2026-07-16 19:17:40 +0330

    cleaned createJson.py file

--- [CAgent_47]omnipkg/core/createJson.py
+++ [CAgent_47]omnipkg/core/createJson.py
@@ -1,8 +1,99 @@
 import os
 import json
-
-#‌def createJsonFileIFNotExists()
-
+pkg_managers_json = {
+    "apt": {
+        "update": "sudo apt update && sudo apt full-upgrade -y",
+        "install": "sudo apt install -y"
+    },
+    "dnf":{
+        "update": "sudo dnf check-update && sudo dnf upgrade -y",
+        "install": "sudo dnf install -y"
+    },
+    "pacman": {
+        "update": "sudo pacman -Syu --noconfirm",
+        "install": "sudo pacman -S --noconfirm"
+    },
+    "yum":{
+        "update": "sudo yum check-update && sudo yum update -y",
+        "install": "sudo yum install -y"
+    },
+    "zypper": {
+        "update": "sudo zypper refresh && sudo zypper update -y",
+        "install": "sudo zypper install -y"
+    },
+    "apk": {
+        "update": "sudo apk update && sudo apk upgrade",
+        "install": "sudo apk add"
+    },
+    "xbps-install":{
+        "update": "sudo xbps-install -S && sudo xbps-install -Su",
+        "install": "sudo xbps-install -y"
+    },
+    "eopkg": {
+        "update": "sudo eopkg update-repo && sudo eopkg upgrade -y",
+        "install": "sudo eopkg install -y"
+    },
+    "emerge": {
+        "update": "sudo emerge --sync && sudo emerge -avuDN @world",
+        "install": "sudo emerge"
+    },
+    "nix": {
+        "update": "nix-channel --update && nix-env -u",
+        "install": "nix-env -iA nixpkgs."
+    },
+    "guix": {
+        "update": "guix pull && guix package -u",
+        "install": "guix install"
+    },
+    "pkg": {
+        "update": "sudo pkg update && sudo pkg upgrade -y",
+        "install": "sudo pkg install -y"
+    },
+    "brew": {
+        "update": "brew update && brew upgrade",
+        "install": "brew install"
+    },
+    "flatpak": {
+        "update": "flatpak update -y",
+        "install": "flatpak install -y"
+    },
+    "snap": {
+        "update": "sudo snap refresh",
+        "install": "sudo snap install"
+    },
+    "winget": {
+        "update": "winget upgrade --all",
+        "install": "winget install"
+    },
+    "choco": {
+        "update": "choco upgrade all -y",
+        "install": "choco install -y"
+    },
+    "scoop": {
+        "update": "scoop update *",
+        "install": "scoop install"
+    },
+    "pkgin": {
+        "update": "sudo pkgin update && sudo pkgin upgrade",
+        "install": "sudo pkgin install"
+    },
+    "opkg": {
+        "update": "opkg update && opkg upgrade",
+        "install": "opkg install"
+    },
+    "swupd": {
+        "update": "sudo swupd update",
+        "install": "sudo swupd bundle-add"
+    },
+    "urpmi": {
+        "update": "sudo urpmi.update -a && sudo urpmi --auto-update",
+        "install": "sudo urpmi"
+    },
+        "tdnf": {
+        "update": "sudo tdnf check-update && sudo tdnf upgrade -y",
+        "install": "sudo tdnf install -y"
+    }
+}
 pkg_List = {
     "Author INFO":{
         "project": "Linux Package installer",
@@ -300,8 +391,16 @@ pkg_List = {
             "btop"
         ]
 }
-
+pkg_file = 'core/distroPKG.json'
 pkg_configuration = "core/packages.json"
+
+def createJsonFileIFNotExists(file, creater, val):
+    if not os.path.exists(file):
+        createJsonFile(creater, val)
+    else:
+        print("[ Python Message ]: Success")
+
+
 def createJsonFile(file, list):
     with open(file, "w") as savePackages:
         json.dump(list, savePackages, indent=4)
@@ -309,111 +408,5 @@ def createJsonFile(file, list):
     print("[ Python Message ]: The json file did not exist and was created. Edit the config.json file to edit the installable packages.")
     print(" ")
 
-
-if not os.path.exists(pkg_configuration):
-    createJsonFile(pkg_configuration, pkg_List)
-else:
-    print("[ Python Message ]: Success")
-
-
-pkg_managers_json = {
-    "apt": {
-        "update": "sudo apt update && sudo apt full-upgrade -y",
-        "install": "sudo apt install -y"
-    },
-    "dnf":{
-        "update": "sudo dnf check-update && sudo dnf upgrade -y",
-        "install": "sudo dnf install -y"
-    },
-    "pacman": {
-        "update": "sudo pacman -Syu --noconfirm",
-        "install": "sudo pacman -S --noconfirm"
-    },
-    "yum":{
-        "update": "sudo yum check-update && sudo yum update -y",
-        "install": "sudo yum install -y"
-    },
-    "zypper": {
-        "update": "sudo zypper refresh && sudo zypper update -y",
-        "install": "sudo zypper install -y"
-    },
-    "apk": {
-        "update": "sudo apk update && sudo apk upgrade",
-        "install": "sudo apk add"
-    },
-    "xbps-install":{
-        "update": "sudo xbps-install -S && sudo xbps-install -Su",
-        "install": "sudo xbps-install -y"
-    },
-    "eopkg": {
-        "update": "sudo eopkg update-repo && sudo eopkg upgrade -y",
-        "install": "sudo eopkg install -y"
-    },
-    "emerge": {
-        "update": "sudo emerge --sync && sudo emerge -avuDN @world",
-        "install": "sudo emerge"
-    },
-    "nix": {
-        "update": "nix-channel --update && nix-env -u",
-        "install": "nix-env -iA nixpkgs."
-    },
-    "guix": {
-        "update": "guix pull && guix package -u",
-        "install": "guix install"
-    },
-    "pkg": {
-        "update": "sudo pkg update && sudo pkg upgrade -y",
-        "install": "sudo pkg install -y"
-    },
-    "brew": {
-        "update": "brew update && brew upgrade",
-        "install": "brew install"
-    },
-    "flatpak": {
-        "update": "flatpak update -y",
-        "install": "flatpak install -y"
-    },
-    "snap": {
-        "update": "sudo snap refresh",
-        "install": "sudo snap install"
-    },
-    "winget": {
-        "update": "winget upgrade --all",
-        "install": "winget install"
-    },
-    "choco": {
-        "update": "choco upgrade all -y",
-        "install": "choco install -y"
-    },
-    "scoop": {
-        "update": "scoop update *",
-        "install": "scoop install"
-    },
-    "pkgin": {
-        "update": "sudo pkgin update && sudo pkgin upgrade",
-        "install": "sudo pkgin install"
-    },
-    "opkg": {
-        "update": "opkg update && opkg upgrade",
-        "install": "opkg install"
-    },
-    "swupd": {
-        "update": "sudo swupd update",
-        "install": "sudo swupd bundle-add"
-    },
-    "urpmi": {
-        "update": "sudo urpmi.update -a && sudo urpmi --auto-update",
-        "install": "sudo urpmi"
-    },
-        "tdnf": {
-        "update": "sudo tdnf check-update && sudo tdnf upgrade -y",
-        "install": "sudo tdnf install -y"
-    }
-}
-
-pkg_file = 'core/distroPKG.json'
-
-if not os.path.exists(pkg_file):
-    createJsonFile(pkg_file, pkg_managers_json)
-else:
-    print("[ Python Message ]: Success")
+createJsonFileIFNotExists(pkg_configuration, pkg_configuration, pkg_List)
+createJsonFileIFNotExists(pkg_file, pkg_file, pkg_managers_json)
